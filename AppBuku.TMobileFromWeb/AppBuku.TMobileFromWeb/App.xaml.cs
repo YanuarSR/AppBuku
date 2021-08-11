8d24cacdfe710716602a0661b3894f99b9391bb4
YanuarSR <yanuarsahrulramadhan02@gmail.com>
2021-08-11 21:55:19 +0700

    first commit

---
+++ AppBuku.TMobileFromWeb/AppBuku.TMobileFromWeb/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AppBuku.TMobileFromWeb.Services;
+using AppBuku.TMobileFromWeb.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBuku.TMobileFromWeb
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            // Set properties untuk aplikasi  
+            this.Properties.Add("BaseWebUri", "http://appbuku.somee.com/");
+            this.Properties.Add("WebUsername", "u023yanuar");
+            this.Properties.Add("WebPassword", "u023yan-@aqw59");
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
